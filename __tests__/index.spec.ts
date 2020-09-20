94e4cb492de279bf70f6156e390244f8ff25e31c
Jakub Biesiada <biesiadajakub@icloud.com>
2020-09-20 08:36:10 +0200

    test: test regexp in input (#10)

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,11 +1,12 @@
 import sTrimmer from '../src';
 
 describe('sTrimmer', () => {
-  it('should remove unneeded spaces', () => {
-    const input = 'lorem  ipsum dolor  ';
+  it.each(['lorem  ipsum dolor  ', '\n\rlorem  ipsum\t dolor  \n'])(
+    'should remove unneeded spaces from: %s',
+    (input) => {
+      const value = sTrimmer(input);
 
-    const value = sTrimmer(input);
-
-    expect(value).toBe('lorem ipsum dolor');
-  });
+      expect(value).toBe('lorem ipsum dolor');
+    }
+  );
 });
