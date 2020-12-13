f8ec01d10c74dd1061703fdd917244c8d014916e
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-13 15:40:31 +0100

    feat: add trimStart, trimEnd & trimCenter functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+export const trimStart = (value: string) => value.replace(/^\s+/, '');
+
+export const trimEnd = (value: string) => value.replace(/\s+$/, '');
+
+export const trimCenter = (value: string) => value.split(/\s+/).join(' ');
+
 const sTrimmer = (value: string) => {
-  return value.trim().split(/\s+/).join(' ');
+  return trimCenter(trimEnd(trimStart(value)));
 };
 
 export default sTrimmer;
