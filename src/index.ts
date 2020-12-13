eb16a9303791c78394c4bff62f0b312393e5b5c6
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-13 15:57:34 +0100

    refactor: clean up

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ export const trimEnd = (value: string) => value.replace(/\s+$/, '');
 
 export const trimCenter = (value: string) => value.split(/\s+/).join(' ');
 
-const sTrimmer = (value: string) => {
-  return trimCenter(trimEnd(trimStart(value)));
-};
+const sTrimmer = (value: string) => trimCenter(trimEnd(trimStart(value)));
 
 export default sTrimmer;
