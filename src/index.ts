1a27c387e7e7f10124691eb4436dfef8061f17d1
Jakub Biesiada <biesiadajakub@icloud.com>
2021-02-05 20:17:43 +0100

    feat: add alias for start, end, between

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 export const trimStart = (value: string) => value.replace(/^\s+/, '');
+export const trimLeft = trimStart;
 
 export const trimEnd = (value: string) => value.replace(/\s+$/, '');
+export const trimRight = trimEnd;
 
-export const trimCenter = (value: string) => value.split(/\s+/).join(' ');
+export const trimBetween = (value: string) => value.replace(/\d+\*s+/g, ' ');
+export const trimCenter = trimBetween;
 
-const sTrimmer = (value: string) => trimCenter(trimEnd(trimStart(value)));
+const sTrimmer = (value: string) => trimBetween(trimEnd(trimStart(value)));
 
 export default sTrimmer;
