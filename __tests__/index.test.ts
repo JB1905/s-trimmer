f3b2748f7556006639b65853c5a166a9de73c16f
Jakub Biesiada <kbba@op.pl>
2021-05-01 20:40:35 +0200

    feat: add trimStart, trimEnd & trimCenter functions (#16)

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,4 +1,4 @@
-import sTrimmer from '../src';
+import sTrimmer, { trimStart, trimEnd, trimCenter } from '../src';
 
 describe('sTrimmer', () => {
   it.each(['lorem  ipsum dolor  ', '\n\rlorem  ipsum\t dolor  \n'])(
@@ -9,4 +9,37 @@ describe('sTrimmer', () => {
       expect(value).toBe('lorem ipsum dolor');
     }
   );
+});
+
+describe('trimStart/trimLeft', () => {
+  it.each([
+    ['lorem  ipsum dolor  ', 'lorem  ipsum dolor  '],
+    ['\n\rlorem  ipsum\t dolor  \n', 'lorem  ipsum\t dolor  \n'],
+  ])('should remove unneeded spaces from: %s', (input, x) => {
+    const value = trimStart(input);
+
+    expect(value).toBe(x);
+  });
+});
+
+describe('trimEnd/trimRight', () => {
+  it.each([
+    ['lorem  ipsum dolor  ', 'lorem  ipsum dolor'],
+    ['\n\rlorem  ipsum\t dolor  \n', '\n\rlorem  ipsum\t dolor'],
+  ])('should remove unneeded spaces from: %s', (input, x) => {
+    const value = trimEnd(input);
+
+    expect(value).toBe(x);
+  });
+});
+
+describe('trimBetween/trimCenter', () => {
+  it.each([
+    ['lorem  ipsum dolor  ', 'lorem ipsum dolor  '],
+    ['\n\rlorem  ipsum\t dolor  \n', '\n\rlorem ipsum dolor  \n'],
+  ])('should remove unneeded spaces from: %s', (input, x) => {
+    const value = trimCenter(input);
+
+    expect(value).toBe(x);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-const sTrimmer = (value: string) => {
-  return value.trim().split(/\s+/).join(' ');
-};
+export const trimStart = (value: string) => value.replace(/^\s+/, '');
+export const trimLeft = trimStart;
+
+export const trimEnd = (value: string) => value.replace(/\s+$/, '');
+export const trimRight = trimEnd;
+
+export const trimBetween = (value: string) =>
+  value.replace(/(?<=\S+)\s+(?=\S+)/g, ' ');
+export const trimCenter = trimBetween;
+
+const sTrimmer = (value: string) => trimBetween(trimEnd(trimStart(value)));
 
 export default sTrimmer;
