cf26f42ef453d8ba0bfdeec204687aecf89fd424
Jakub Biesiada <biesiadajakub@icloud.com>
2021-05-01 20:35:42 +0200

    fix: update trimBetween

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ export const trimLeft = trimStart;
 export const trimEnd = (value: string) => value.replace(/\s+$/, '');
 export const trimRight = trimEnd;
 
-export const trimBetween = (value: string) => value.split(/\s/);
+export const trimBetween = (value: string) =>
+  value.replace(/(?<=\S+)\s+(?=\S+)/g, ' ');
 export const trimCenter = trimBetween;
 
 const sTrimmer = (value: string) => trimBetween(trimEnd(trimStart(value)));
