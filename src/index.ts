f74fcdc30a8adc36b4f765b33034d97b84a9cbd0
Jakub Biesiada <biesiadajakub@icloud.com>
2021-09-27 22:56:57 +0200

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ const START_PATTERN = /^\s+/;
 export const trimStart = (value: string) => value.replace(START_PATTERN, '');
 export const trimLeft = trimStart;
 
-const END_PATTERN = /\s+$/
+const END_PATTERN = /\s+$/;
 export const trimEnd = (value: string) => value.replace(END_PATTERN, '');
 export const trimRight = trimEnd;
 
-const CENTER_PATTERN = /(?<=\S+)\s+(?=\S+)/g
-export const trimBetween = (value: string) => value.replace(CENTER_PATTERN, ' ');
+const CENTER_PATTERN = /(?<=\S+)\s+(?=\S+)/g;
+export const trimBetween = (value: string) =>
+  value.replace(CENTER_PATTERN, ' ');
 export const trimCenter = trimBetween;
 
 const sTrimmer = (value: string) => trimBetween(trimEnd(trimStart(value)));
